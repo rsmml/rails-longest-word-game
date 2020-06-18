81223abe47ca738a1208fa87d8a95800d6986d54
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-06-18 20:12:53 +0200

    challeng done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,9 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @letters = generate_grid
-  end
-
-  def score
   end
 
   def generate_letter
@@ -14,5 +14,30 @@ class GamesController < ApplicationController
     grid_array = []
     grid_array.push(generate_letter) while grid_array.count < 10
     grid_array
+  end
+
+  def included?
+    @word.chars.sort.all? { |letter| @grid.include?(letter) }
+  end
+
+  def english_word?
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    word_dictionary = open(url).read
+    word = JSON.parse(word_dictionary)
+    word['found']
+  end
+
+  def score
+    # raise
+    @grid = params[:grid]
+    @word = params[:word].upcase
+
+    grid_letters = @grid.each_char { |letter| print letter, '' }
+
+    included? ?
+      english_word? ?
+        @result = "Congratulation! #{@word} is a valid English word." :
+        @result = "Sorry but #{@word} is not an English word."
+      : @result = "Sorry, but #{@word} can’t be built out of #{grid_letters}."
   end
 end
